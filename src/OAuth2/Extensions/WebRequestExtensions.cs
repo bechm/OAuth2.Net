8c00ddfd565881d67dc94e8d00067f9ed502c6d4
Rene Stoeckmann <rene.stoeckmann@web.de>
2011-11-06 13:08:55 +0100

    added Project OAuth2, add gitignore with Rules for OAuth2

---
+++ src/OAuth2/Extensions/WebRequestExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Net;
+
+namespace NNS.Authentication.OAuth2.Extensions
+{
+    public static class WebRequestExtensions
+    {
+        public static void SignRequest(this WebRequest webRequest)
+        {
+            
+        }
+    }
+}
